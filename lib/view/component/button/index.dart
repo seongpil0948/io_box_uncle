de0cd1696ba9fa8a66481bb7e1ff9e65cf155ac8
seongpil0948 <seongpil0948@gmail.com>
2022-09-05 15:21:23 +0900

    show ship state confirm

--- lib/view/component/button/index.dart
+++ lib/view/component/button/index.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:io_box_uncle/module/app/bloc/app_bloc.dart';
--- lib/view/component/button/ship_status_button.dart
+++ lib/view/component/button/ship_status_button.dart
@@ -6,29 +6,37 @@ List<Widget> shipStatusBtns(BuildContext context, ShipOrder p) {
     if (p.order.state == OrderState.beforePickup)
       OutlinedButton(
           onPressed: () {
-            shipBloc.add(StartPickup(shipOrder: p));
-            context.read<AppBloc>().add(DisSelectPickup());
+            showConfirmClose(context, () {
+              shipBloc.add(StartPickup(shipOrder: p));
+              context.read<AppBloc>().add(DisSelectPickup());
+            }, "픽업시작 처리 하시겠습니까?");
           },
           child: const Text("픽업시작")),
     if (p.order.state == OrderState.ongoingPickup)
       OutlinedButton(
           onPressed: () {
-            shipBloc.add(DonePickup(shipOrder: p));
-            context.read<AppBloc>().add(DisSelectPickup());
+            showConfirmClose(context, () {
+              shipBloc.add(DonePickup(shipOrder: p));
+              context.read<AppBloc>().add(DisSelectPickup());
+            }, "픽업완료 처리 하시겠습니까?");
           },
           child: const Text("픽업완료")),
     if (p.order.state == OrderState.pickupComplete)
       OutlinedButton(
           onPressed: () {
-            shipBloc.add(ToBeforeShip(shipOrder: p));
-            context.read<AppBloc>().add(DisSelectPickup());
+            showConfirmClose(context, () {
+              shipBloc.add(ToBeforeShip(shipOrder: p));
+              context.read<AppBloc>().add(DisSelectPickup());
+            }, "배송전 처리 하시겠습니까?");
           },
           child: const Text("배송전")),
     if (p.order.state == OrderState.beforeShip)
       OutlinedButton(
           onPressed: () {
-            shipBloc.add(StartShip(shipOrder: p));
-            context.read<AppBloc>().add(DisSelectPickup());
+            showConfirmClose(context, () {
+              shipBloc.add(StartShip(shipOrder: p));
+              context.read<AppBloc>().add(DisSelectPickup());
+            }, "배송시작 처리 하시겠습니까?");
           },
           child: const Text("배송시작")),
     OutlinedButton(
@@ -39,9 +47,34 @@ List<Widget> shipStatusBtns(BuildContext context, ShipOrder p) {
     if (p.order.state == OrderState.shipping)
       OutlinedButton(
           onPressed: () {
-            shipBloc.add(DoneShip(shipOrder: p));
-            context.read<AppBloc>().add(DisSelectPickup());
+            showConfirmClose(context, () {
+              shipBloc.add(DoneShip(shipOrder: p));
+              context.read<AppBloc>().add(DisSelectPickup());
+            }, "배송완료 처리 하시겠습니까?");
           },
           child: const Text("배송완료")),
   ];
+}
+
+Future showConfirmClose(
+    BuildContext context, void Function() onConfirm, String content) {
+  return showDialog(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+            title: const Text("배송상태 변경"),
+            content: Text(content),
+            actions: [
+              CupertinoDialogAction(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("취소")),
+              CupertinoDialogAction(
+                  onPressed: () {
+                    onConfirm();
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("승인")),
+            ],
+          ));
 }
