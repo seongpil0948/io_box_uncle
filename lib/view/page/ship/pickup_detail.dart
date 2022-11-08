cb5e3fc8ecfa5c5cb998123253ab5f2001315c29
seongpil0948 <seongpil0948@gmail.com>
2022-11-08 11:29:11 +0900

    ui 짤림현상

--- lib/view/page/ship/pickup_detail.dart
+++ lib/view/page/ship/pickup_detail.dart
@@ -35,22 +35,26 @@ class PickupDetailPage extends StatelessWidget {
               ShipAmountCard(p: p),
               const SizedBox(height: 10),
               // 상품명, 컬러, 사이즈, 수량
-              DataTable(columns: [
-                DataColumn(label: Text(p.order.isPickup ? '도매처명' : "소매처명")),
-                const DataColumn(label: Text('상품명')),
-                const DataColumn(label: Text('컬러')),
-                const DataColumn(label: Text('사이즈')),
-                const DataColumn(label: Text('수량'), numeric: true),
-              ], rows: [
-                DataRow(cells: [
-                  DataCell(Text(
-                      p.order.isPickup ? p.vendorUser.name : p.shopUser.name)),
-                  DataCell(Text(p.vendorGarment.vendorProdName)),
-                  DataCell(Text(p.vendorGarment.color)),
-                  DataCell(Text(p.vendorGarment.size)),
-                  DataCell(Text(p.order.activeCnt.toString())),
-                ])
-              ]),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: DataTable(columns: [
+                  DataColumn(label: Text(p.order.isPickup ? '도매처명' : "소매처명")),
+                  const DataColumn(label: Text('상품명')),
+                  const DataColumn(label: Text('컬러')),
+                  const DataColumn(label: Text('사이즈')),
+                  const DataColumn(label: Text('수량'), numeric: true),
+                ], rows: [
+                  DataRow(cells: [
+                    DataCell(Text(p.order.isPickup
+                        ? p.vendorUser.name
+                        : p.shopUser.name)),
+                    DataCell(Text(p.vendorGarment.vendorProdName)),
+                    DataCell(Text(p.vendorGarment.color)),
+                    DataCell(Text(p.vendorGarment.size)),
+                    DataCell(Text(p.order.activeCnt.toString())),
+                  ])
+                ]),
+              ),
             ],
           ),
         ),
